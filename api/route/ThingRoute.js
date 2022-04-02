0bfa47db0adeb6c07036f49ac366edb534a5c3ab
Philippe Beck <philippe@philippebeck.net>
2022-04-02 13:40:20 +0200

    Create ThingRoute

---
+++ api/route/ThingRoute.js
@@ -0,0 +1,17 @@
+"use strict";
+
+const express = require("express");
+const router = express.Router();
+
+const auth    = require("../middleware/auth");
+const multer  = require("../middleware/multer");
+
+const ThingCtrl = require("../controller/ThingCtrl");
+
+router.get("/", auth, ThingCtrl.listThing);
+router.post("/", auth, multer, ThingCtrl.createThing);
+router.get("/:id", auth, ThingCtrl.readThing);
+router.put("/:id", auth, multer, ThingCtrl.updateThing);
+router.delete("/:id", auth, ThingCtrl.deleteThing);
+
+module.exports = router;
