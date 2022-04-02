011cfaee1af69d355a6a05fb1ccafd26c0024c52
Philippe Beck <philippe@philippebeck.net>
2022-04-02 13:39:06 +0200

    Create ThingModel

---
+++ api/model/ThingModel.js
@@ -0,0 +1,28 @@
+"use strict";
+
+const mongoose = require("mongoose");
+
+const ThingModel = mongoose.Schema({
+  title: { 
+    type: String, 
+    required: true 
+  },
+  description: { 
+    type: String, 
+    required: true 
+  },
+  imageUrl: { 
+    type: String, 
+    required: true 
+  },
+  userId: { 
+    type: String, 
+    required: true 
+  },
+  price: { 
+    type: Number, 
+    required: true 
+  }
+});
+
+module.exports = mongoose.model("Thing", ThingModel);
