cbdfee0bbc5379bacb88a7a4d1e56dcb463af8de
Philippe Beck <philippe@philippebeck.net>
2022-04-03 18:51:11 +0200

    Create CreateResourceView

---
+++ ui/src/views/CreateResourceView.vue
@@ -0,0 +1,150 @@
+<template>
+  <form class="form" 
+    method="post" 
+    action="#">
+    <fieldset>
+      <legend>
+        <i class="fas fa-box-open fa-2x"></i>
+      </legend>
+      <ul>
+        <li>
+          <label for="name">
+            Nom
+          </label>
+          <input 
+            id="name" 
+            name="name" 
+            type="text" 
+            maxlength="30" 
+            placeholder="Insérer le Nom"
+            required>
+        </li>
+        <li>
+          <label for="link">
+            Lien
+          </label>
+          <input 
+            id="link" 
+            name="link" 
+            type="text" 
+            maxlength="100" 
+            placeholder="https://"
+            required>
+        </li>
+        <li>
+          <label for="technology">
+            Technologie
+          </label>
+          <select 
+            id="technology" 
+            name="technology" 
+            title="Choisissez la Technologie"
+            required>
+            <option value="">
+              Choisissez une Technologie
+            </option>
+            <option value="HTML">
+              HTML
+            </option>
+            <option value="CSS">
+              CSS
+            </option>
+            <option value="JS">
+              JavaScript
+            </option>
+            <option value="PHP">
+              PHP
+            </option>
+            <option value="SQL">
+              SQL
+            </option>
+            <option value="Git">
+              Git
+            </option>
+            <option value="Media">
+              Media
+            </option>
+          </select>
+        </li>
+        <li>
+          <label for="category">
+            Catégorie
+          </label>
+          <input 
+            id="category" 
+            name="category" 
+            type="text" 
+            maxlength="30" 
+            placeholder="Insérer la Catégorie"
+            required>
+        </li>
+        <li>
+          <label for="description">
+            Description
+          </label>
+          <textarea 
+            id="description" 
+            name="description" 
+            rows="5" 
+            cols="30" 
+            placeholder="Insérer la Description"
+            required></textarea>
+        </li>
+      </ul>
+      <ul>
+        <li>
+          <input 
+            type="reset" 
+            value="Réinitialiser">
+          <button 
+            type="button" 
+            @click='createResource()' 
+            class="btn-green">
+            Create
+          </button>
+        </li>
+      </ul>
+    </fieldset>
+    <a 
+      class="btn-black btn-lg" 
+      href="#">
+      Quitter
+    </a>
+  </form>
+</template>
+
+<script>
+  export default {
+    name: 'CreateResource',
+    data() {
+      return {
+        name: '',
+        link: '',
+        technology: '',
+        category: '',
+        description: ''
+      }
+    },
+    methods: {
+      createResource() {
+        console.log("createResource()", this.name)
+        const payload = {
+          name: this.name,
+          link: this.link,
+          technology: this.technology,
+          category: this.category,
+          description: this.description
+        }
+        this.$emit('createResource', payload)
+        this.clearForm();
+      },
+      clearForm() {
+        this.name = "";
+        this.link = "";
+        this.technology = "";
+        this.category = "";
+        this.description = "";
+      }
+    }
+  }
+</script>
