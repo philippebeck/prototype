067eebd9e59961bf59026ce54909deeb11b54bfb
Philippe Beck <philippe@philippebeck.net>
2022-04-03 18:24:41 +0200

    Create ResourceService

---
+++ ui/src/services/ResourceService.js
@@ -0,0 +1,15 @@
+"mode strict";
+
+const axios = require('axios');
+
+export async function getAllResources() {
+    const response = await axios.get('/api/resources');
+
+    return response.data;
+}
+
+export async function createResource(data) {
+    const response = await axios.post('/api/resource', { resource: data });
+
+    return response.data;
+}
