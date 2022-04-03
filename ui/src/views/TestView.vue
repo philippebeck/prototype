412f9bcf27f3d6d8c61fc2dbc1a9381d18c78c27
Philippe Beck <philippe@philippebeck.net>
2022-04-03 16:57:41 +0200

    Create TestView

---
+++ ui/src/views/TestView.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="test">
+    <h1>This is the test page</h1>
+    <DashBoard v-if="settings == true"/>
+  </div>
+</template>
+
+<script>
+import DashBoard from '@/components/DashBoard.vue'
+
+export default {
+  name: 'TestView',
+  components: {
+    DashBoard
+  },
+  data() {
+      return {
+          settings: false
+      }
+  },
+  mounted() {
+    console.log("I am in mounted!!!")
+    this.settings = true
+  },
+  setup () {
+    console.log("I am in setup!!!")
+  }
+}
+</script>
