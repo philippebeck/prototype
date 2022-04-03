cb29aff8ffbd5b98124a82a7c1f2ba6a976f2baf
Philippe Beck <philippe@philippebeck.net>
2022-04-03 14:16:03 +0200

    Test DashBoard to MainView

--- ui/src/views/MainView.vue
+++ ui/src/views/MainView.vue
@@ -1,5 +1,29 @@
 <template>
   <div class="main">
     <h1>This is the main page</h1>
+    <DashBoard v-if="settings == true"/>
   </div>
 </template>
+
+<script>
+import DashBoard from '@/components/DashBoard.vue'
+
+export default {
+  name: 'MainView',
+  components: {
+    DashBoard
+  },
+  data() {
+      return {
+          settings: false
+      }
+  },
+  mounted() {
+    console.log("I am in mounted!!!")
+    this.settings = true
+  },
+  setup () {
+    console.log("I am in setup!!!")
+  }
+}
+</script>
