126eeea5d578fda29e7b26a9c3637c62859a4cd5
Philippe Beck <philippe@philippebeck.net>
2022-04-03 18:25:26 +0200

    Add resource script to AdminView

--- ui/src/views/AdminView.vue
+++ ui/src/views/AdminView.vue
@@ -34,7 +34,9 @@
     <section class="menu-relay">
       <h2>Datas</h2>
 
-      <ListResources/>
+      <ListResources 
+        v-if="resources.length > 0" 
+        :resources="resources"/>
 
       <ListUsers 
         v-if="users.length > 0" 
@@ -47,6 +49,7 @@
 import ListResources from '@/components/ListResources.vue'
 import ListUsers from '@/components/ListUsers.vue'
 import { getAllUsers, createUser } from '../services/UserService'
+import { getAllResources, createResource } from '../services/ResourceService'
 
 export default {
   name: 'AdminView',
@@ -55,12 +58,28 @@ export default {
     ListUsers
   },
   data() {
-      return {
-          users: [],
-          numberOfUsers: 0
-      }
+    return {
+      users: [],
+      numberOfUsers: 0,
+      resources: [],
+      numberOfResources: 0
+    }
   },
   methods: {
+    getAllResources() {
+      getAllResources().then(response => {
+        console.log("getAllResources()", response)
+        this.resources = response
+        this.numberOfResources = this.resources.length
+      })
+    },
+    resourceCreate(data) {
+      console.log("resourceCreate(data)", data)
+      createResource(data).then(response => {
+        console.log(response);
+        this.getAllResources();
+      });
+    },
     getAllUsers() {
       getAllUsers().then(response => {
         console.log("getAllUsers()", response)
@@ -78,6 +97,7 @@ export default {
   },
   mounted () {
     this.getAllUsers();
+    this.getAllResources();
   }
 }
 </script>
