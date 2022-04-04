30acb73deeae507cd74de47c1f7b77a808c5761b
Philippe Beck <philippe@philippebeck.net>
2022-04-04 19:32:26 +0200

    Switch axios to fetch to CreateUserView

--- ui/src/components/CreateUserView.vue
+++ ui/src/components/CreateUserView.vue
@@ -22,16 +22,6 @@
             placeholder="Insérer le Nom" 
             required>
         </li>
-        <!--<li>
-          <label for="file">
-            Photo (max 5Mo)
-          </label>
-          <input 
-            name="file" 
-            type="file" 
-            accept=".jpg, .jpeg, .png, .svg, .gif" 
-            required>
-        </li>-->
         <li>
           <label for="email">
             Courriel
@@ -90,34 +80,68 @@
 </template>
 
 <script>
-  export default {
-    name: 'CreateUser',
-    data() {
-      return {
-        name: '',
-        //file: '',
-        email: '',
-        pass: ''
-      }
-    },
-    methods: {
-      createUser() {
-        console.log("createUser()", this.name)
-        const payload = {
+export default {
+  name: 'CreateUserView',
+  data() {
+    return {
+      name: '',
+      email: '',
+      pass: '',
+    }
+  },
+  methods: {
+    createUser() {
+      let data = {
           name: this.name,
-          //file: this.file,
           email: this.email,
           pass: this.pass
-        }
-        this.$emit('createUser', payload)
-        this.clearForm();
-      },
-      clearForm() {
-        this.name = "";
-        //this.file = "";
-        this.email = "";
-        this.pass = "";
+      };
+
+      const regexText = /^[a-zA-Z-\s]+$/;
+      const regexEmail = /^[a-zA-Z0-9.-_]+[@]{1}[a-zA-Z0-9.-_]+[.]{1}[a-z]{2,10}$/; // eslint-disable-line
+      const regexPassword = /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{3,50}$/;
+
+      if (this.name === "") {
+          alert("Veuillez remplir votre nom");
+      } else if (regexText.test(this.name) === false) {
+          alert("Veuillez vérifier que l'écriture de votre nom soit uniquement en lettre");}
+      
+      if (this.email === "") {
+          alert("Veuillez remplir votre adresse email");
+      } else if (regexEmail.test(this.email) === false) {
+          alert("Veuillez écrire une adresse email valide");}
+
+      if (this.pass === "") {
+          alert("Veuillez remplir votre mot de passe");
+      } else if (regexPassword.test(this.pass) === false) {
+          alert("Veuillez vérifier l'écriture de votre mot de passe, il doit contenir au moins une majuscule, une minuscule ainsi qu'un chiffre");
+      }else if ((regexText.test(this.name) === true) && regexEmail.test(this.email) === true && regexPassword.test(this.pass) === true ) {
+
+          fetch("http://localhost:3000/api/auth/signup", {
+              method: "POST",
+              headers: {
+              'Accept': 'application/json',
+              'Content-Type': 'application/json'
+              },
+              body: JSON.stringify(data)
+          })
+          .then(response => {
+            if(response.ok) {
+              return response.json()
+            } else {
+              return response.text()
+              .then((text) => {
+                throw new Error(text)}
+              )
+            }
+          })  
+          .then(() => {
+              alert("Votre inscription est bien prise en compte");
+              this.$router.push("/admin");
+          })
+          .catch(alert)
       }
     }
   }
+}
 </script>
