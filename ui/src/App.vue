6c4f2eb2f156beaec6a1454e4117541cc47317db
Philippe Beck <philippe@philippebeck.net>
2022-04-05 12:04:17 +0200

    Add main style & fix nav url

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <nav>
     <router-link to="/">Home</router-link> |
-    <router-link to="/main">Main</router-link> |
+    <router-link to="/resource">Resource</router-link> |
     <router-link to="/contact">Contact</router-link> |
     <router-link to="/login">Login</router-link>
   </nav>
@@ -9,6 +9,23 @@
 </template>
 
 <style lang="scss">
+:root {
+  --sans-serif: 'Geo', sans-serif;
+  --serif: 'Rozha One', serif;
+  --monospace: 'Syne Mono', monospace;
+
+  --primary: rgb(119, 123, 179); /* PHP */
+  --primary-dark: rgba(119, 123, 179, var(--opacity-dark));
+  --primary-light: rgba(119, 123, 179, var(--opacity-light));
+  --secondary: rgb(50, 51, 48); /* JS */
+  --secondary-dark: rgba(50, 51, 48, var(--opacity-dark));
+  --secondary-light: rgba(50, 51, 48, var(--opacity-light));
+
+  --red: rgb(227, 76, 38); /* HTML */
+  --blue: rgb(38, 77, 228); /* CSS */
+  --cyan: rgb(0, 117, 143); /* SQL */
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -28,5 +45,21 @@ nav {
       color: #42b983;
     }
   }
+}
+
+h1,
+h2 {
+  --title-color: var(--secondary);
+}
+
+.head {
+  --head-border: thick solid var(--secondary);
+  --head-color: var(--cyan);
+}
+
+.table {
+  --table-even-background-color: var(--white);
+  --table-odd-color: var(--black);
+  --table-row-hover-color: var(--white);
 }
 </style>
