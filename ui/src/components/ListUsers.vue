98cb9424faeaabd0ee95891b80f25c349843333b
Philippe Beck <philippe@philippebeck.net>
2022-04-07 12:11:43 +0200

    Switch listUSers table to forms & create updateUser()

--- ui/src/components/ListUsers.vue
+++ ui/src/components/ListUsers.vue
@@ -1,49 +1,108 @@
 <template>
-  <table class="table">
-    <thead>
-      <tr>
-        <th>Utilisateurs</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr 
-        v-for="user in users" 
+  <form class="form">
+    <fieldset>
+      <legend>
+        <i class="fas fa-users fa-lg"></i>
+        Édition d'un Utilisateur
+      </legend>
+      <ul v-for="user in users" 
         :key="user.id">
-        <td>
-          <figure>
-            <img 
-              class="bord-violet" 
-              src="" 
-              alt="Photo">
-            <figcaption>
-              {{ user.name }}
-            </figcaption>
-          </figure>
-          <a 
-            class="btn-blue" 
-            href="#" 
-            title="Modifier l'Utilisateur">
-            <i class="fas fa-pen-square fa-fw fa-lg"></i>
-          </a>
-          <a 
-            class="btn-red" 
-            href="#" 
-            title="Supprimer l'Utilisateur">
-            <i class="fas fa-times-circle fa-fw fa-lg"></i>
-          </a>
-          <br>
-          <p>
-            {{ user.email }}
-          </p>
-        </td>
-      </tr>
-    </tbody>
-  </table>
+        <li>
+          <label for="name">
+            Nom
+          </label>
+          <input 
+            id="name" 
+            name="name" 
+            v-model="user.name" 
+            type="text" 
+            minlength="2" 
+            maxlength="50" 
+            placeholder="Fill in the name" 
+            required>
+        </li>
+        <li>
+          <label for="email">
+            Email
+          </label>
+          <input 
+            id="email" 
+            name="email" 
+            v-model="user.email"
+            type="email" 
+            maxlength="50" 
+            minlength="5" 
+            placeholder="Fill in the email"
+            required>
+        </li>
+        <li>
+          <label for="pass">
+            Password
+          </label>
+          <input 
+            id="pass" 
+            name="pass" 
+            type="password" 
+            placeholder="Fill in the password" 
+            maxlength="50"
+            minlength="8" 
+            required>
+        </li>
+        <li>
+          <button 
+            type="button" 
+            @click="updateUser()"
+            class="btn-green">
+            Update {{ user.name }}
+          </button>
+        </li>
+      </ul>
+    </fieldset>
+  </form>
 </template>
 
 <script>
 export default {
   name: 'ListUsers',
-  props: ['users']
+
+  props: ['users'],
+
+    methods: {
+    updateUser() {
+      let user = {
+        id: this._id,
+        name: this.name,
+        email: this.email,
+        pass: this.pass
+      }
+
+      fetch(`http://localhost:3000/api/users/${user.id}`, {
+          method: "PUT",
+          headers: {
+          "Accept": "application/json",
+          "Content-Type": "application/json"
+          },
+          body: JSON.stringify(user)
+        })
+
+        .then(response => {
+
+          if(response.ok) {
+            return response.json()
+
+          } else {
+            return response.text()
+
+            .then((text) => {
+              throw new Error(text)}
+            )
+          }
+        })
+        .then(() => {
+          alert("Utilisateur mis à jour avec succès");
+        })
+        .catch(alert)
+    }
+  }
 }
 </script>
