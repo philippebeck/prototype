fb474e2cccd8cab8cf9159d8989f6d21c02efcd9
Philippe Beck <philippe@philippebeck.net>
2022-04-07 12:09:18 +0200

    Remove useless file managing to UserCtrl

--- api/controller/UserCtrl.js
+++ api/controller/UserCtrl.js
@@ -105,15 +105,10 @@ exports.login = (req, res) => {
  * @param {object} res 
  */
 exports.update = (req, res) => {
-  const userObject = req.file ?
-    {
-      ...JSON.parse(req.body.user),
-      image: `${req.protocol}://${req.get("host")}/${process.env.IMG}/${req.file.filename}`
-    }
-    : { ...req.body };
+  let user = req.body;
     
   UserModel
-    .updateOne({ _id: req.params.id }, { ...userObject, _id: req.params.id })
+    .updateOne({ _id: req.params.id }, { ...user, _id: req.params.id })
     .then(() => res.status(200).json({ message: "Utilisateur modifié !" }))
     .catch(error => res.status(400).json({ error }));
 };
@@ -126,15 +121,7 @@ exports.update = (req, res) => {
 exports.delete = (req, res) => {
   UserModel
     .findOne({ _id: req.params.id })
-    .then(user => {
-      const filename = user.image.split(`/${process.env.IMG}/`)[1];
-
-      fs.unlink(`${process.env.IMG}/${filename}`, () => {
-        UserModel
-          .deleteOne({ _id: req.params.id })
+    .deleteOne({ _id: req.params.id })
     .then(() => res.status(200).json({ message: "Utilisateur supprimé !" }))
-          .catch(error => res.status(400).json({ error }));
-      });
-    })
-    .catch(error => res.status(500).json({ error }));
+    .catch(error => res.status(400).json({ error }));
 };
