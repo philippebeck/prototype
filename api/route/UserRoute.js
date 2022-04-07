1387fbddf2449218960661dc75b458d6f439bdf0
Philippe Beck <philippe@philippebeck.net>
2022-04-07 12:08:33 +0200

    Remove auth for updateUser

--- api/route/UserRoute.js
+++ api/route/UserRoute.js
@@ -9,7 +9,7 @@ const UserCtrl  = require("../controller/UserCtrl");
 router.get("/", UserCtrl.list);
 router.post("/", UserCtrl.create);
 router.post("/login", UserCtrl.login);
-router.put("/:id", auth, UserCtrl.update);
+router.put("/:id", UserCtrl.update);
 router.delete("/:id", auth, UserCtrl.delete);
 
 module.exports = router;
