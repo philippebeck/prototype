8b6ee9b8d2041e2905127fc1c68321a724a81d4d
Philippe Beck <philippe@philippebeck.net>
2022-04-08 20:54:15 +0200

    Switch menu to sidebar to AdminView

--- ui/src/views/AdminView.vue
+++ ui/src/views/AdminView.vue
@@ -1,67 +1,79 @@
 <template>
-  <main class="admin">
+  <main id="admin">
     <NavElt/>
 
-    <input 
-      id="menu-radio1" 
-      class="menu-radio1" 
-      type="radio" 
-      name="menu" 
-      checked>
-    <input 
-      id="menu-radio2" 
-      class="menu-radio2" 
-      type="radio" 
-      name="menu">
-    <nav class="menu">
+    <h1>Admin</h1>
+    <nav class="sidebar">
+      <input 
+        id="sidebar-class"
+        class="sidebar-check" 
+        type="checkbox">
       <label 
-        for="menu-radio1" 
-        title="Voir les Ressources">
-        <i class="fa-solid fa-link"></i>
-        <span>
-          Liens
-        </span>
+        for="sidebar-class"
+        class="sidebar-open" 
+        title="Afficher">
+        <i class="far fa-eye fa-fw"></i>
       </label>
       <label 
-        for="menu-radio2" 
-        title="Voir les Administrateurs">
-        <i class="fa-solid fa-users"></i>
-        <span>
-          Utilisateurs
-        </span>
+        for="sidebar-class"
+        title="Cacher">
+        <i class="far fa-eye-slash fa-fw color-violet"></i>
       </label>
+      <a 
+        href="#links" 
+        title="Créer un lien">
+        <i class="fas fa-link fa-fw color-violet"></i>
+      </a>
+      <a 
+        v-for="(cat, index) in cats" 
+        :key="index"
+        :href="`#${cat}`" 
+        :title="`Gérer les liens ${cat}`">
+        <i :class="`fa-brands fa-${cat} fa-fw color-violet`"></i>
+      </a>
+      <a 
+        href="#user" 
+        title="Créer un utilisateur">
+        <i class="fas fa-user fa-fw color-violet"></i>
+      </a>
+      <a 
+        href="#users" 
+        title="Gérer les utilisateurs">
+        <i class="fas fa-users fa-fw color-violet"></i>
+      </a>
+      <a 
+        href="#admin" 
+        title="Haut de page">
+        <i class="fas fa-chevron-circle-up fa-fw color-violet"></i>
+      </a>
     </nav>
 
-    <h1>Admin</h1>
+    <section>
+      <h2 id="links">
+        <i class="fa-solid fa-link fa-lg"></i>
+        Liens
+      </h2>
 
-    <div class="menu-relay">
-      <section class="menu-target1">
-        <h2>
-          <i class="fa-solid fa-link fa-lg"></i>
-          Liens
-        </h2>
+      <CreateLink />
 
-        <CreateLink />
+      <ListLinks 
+        v-if="links.length > 0" 
+        :links="links"/>
 
-        <ListLinks 
-          v-if="links.length > 0" 
-          :links="links"/>
+    </section>
+    <section>
+      <h2 id="user">
+        <i class="fa-solid fa-users fa-lg"></i>
+        Utilisateurs
+      </h2>
+
+      <CreateUser />
+
+      <ListUsers 
+        v-if="users.length > 0" 
+        :users="users"/>
 
       </section>
-      <section class="menu-target2">
-        <h2>
-          <i class="fa-solid fa-users fa-lg"></i>
-          Utilisateurs
-        </h2>
-
-        <CreateUser />
-
-        <ListUsers 
-          v-if="users.length > 0" 
-          :users="users"/>
-
-        </section>
-    </div>
 
     <FootElt/>
   </main>
@@ -91,6 +103,13 @@ export default {
     return {
       links: [],
       users: []
+    }
+  },
+  computed: {
+    cats() {
+      const cats = new Set();
+      this.links.forEach(link => cats.add(link.cat));
+      return Array.from(cats); 
     }
   },
   methods: {
