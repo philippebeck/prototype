cf8923d2b128c1ea945593f2c0f91b5f5a15b320
Philippe Beck <philippe@philippebeck.net>
2022-04-13 23:25:30 +0200

    Add back Bearer keyword to headers authorization

--- api/middleware/auth.js
+++ api/middleware/auth.js
@@ -7,7 +7,7 @@ require("dotenv").config();
 module.exports = (req, res, next) => {
   
   try {
-    const token     = req.headers.authorization;
+    const token     = req.headers.authorization.split(" ")[1];
     const tokenData = jwt.verify(token, process.env.JWT);
     const userId    = tokenData.userId;
 
