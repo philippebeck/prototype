662fee6e2f3d134032afc6797d7c9a7a8272ed1d
Philippe Beck <philippe@philippebeck.net>
2022-04-13 23:03:14 +0200

    Create new constants for api

--- ui/src/config/constants.js
+++ ui/src/config/constants.js
@@ -13,6 +13,10 @@ export const ALERT_REGEX_URL    = "Indicate a valid url";
 export const ALERT_REGEX_EMAIL  = "Indicate a valid email";
 export const ALERT_REGEX_PASS   = "8 to 50 characters including an uppercase, a lowercase & a number";
 
+export const API_URL  = "http://localhost:3000";
+export const TOKEN    = JSON.parse(localStorage.getItem("userToken"));
+export const USER_ID  = JSON.parse(localStorage.getItem("userId"));
+
 export const REGEX_NAME   = /^[A-zÀ-ÿ].{1,30}$/;
 export const REGEX_TITLE  = /^[A-zÀ-ÿ].{1,30}$/;
 export const REGEX_URL    = /((https?|ftp|ssh|mailto):\/\/)?[a-z0-9\/:%_+.,#?!@&=-]+$/;
@@ -21,5 +25,3 @@ export const REGEX_PASS   = /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{8,50}$/;
 
 //export const REWRITE_NAME = /(?:^|\s|['`‘’.-])[^\x00-\x60^\x7B-\xDF](?!(\s|$))/;
 export const REWRITE_URL  = /(^\w+:|^)\/\//;
-
-export const TOKEN  = JSON.parse(localStorage.getItem("userToken"));
