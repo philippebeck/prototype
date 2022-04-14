cf094cdfc48a9cfc5c2afeb0947f24e32dd9eb85
Philippe Beck <philippe@philippebeck.net>
2022-04-14 21:28:41 +0200

    Create BtnElt

---
+++ ui/src/components/base/BtnElt.vue
@@ -0,0 +1,43 @@
+<template>
+  <button :type="type"
+    :name="name"
+    @click="action">
+    <slot></slot>
+    {{ content }}
+  </button>
+</template>
+
+<script>
+  export default {
+    name: "Button",
+    props: {
+      type: {
+        type: String,
+        default: "button"
+      },
+      name: {
+        type: String
+      },
+      content: {
+        type: String
+      }
+    },
+    methods: {
+      action() {
+        alert("test !")
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  button {
+    background-color: var(--primary);
+    color: var(--secondary);
+  }
+  button:hover,
+  button:focus {
+    background-color: var(--secondary);
+    color: var(--primary);
+  }
+</style>
