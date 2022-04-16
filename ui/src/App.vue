771b1b04f1359954227721d26d542ef702174463
Philippe Beck <philippe@philippebeck.net>
2022-04-16 20:35:29 +0200

    Import perso style & remove useless style from App

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -4,18 +4,3 @@
   </nav>
   <router-view/>
 </template>
-
-<style lang="scss">
-:root {
-  --sans-serif: 'Geo', sans-serif;
-  --serif: 'Rozha One', serif;
-  --monospace: 'Syne Mono', monospace;
-
-  --primary: rgb(119, 123, 179); /* PHP */
-  --primary-dark: rgba(119, 123, 179, var(--opacity-dark));
-  --primary-light: rgba(119, 123, 179, var(--opacity-light));
-  --secondary: rgb(50, 51, 48); /* JS */
-  --secondary-dark: rgba(50, 51, 48, var(--opacity-dark));
-  --secondary-light: rgba(50, 51, 48, var(--opacity-light));
-}
-</style>
--- ui/src/main.js
+++ ui/src/main.js
@@ -1,7 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+
 import router from './router'
 import store from './store'
+
 import "./style/global.scss"
+import "./style/style.css"
 
 createApp(App).use(store).use(router).mount('#app')
