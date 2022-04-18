c64ef45a6a30777ac263a60b4fae0230915bcb29
Philippe Beck <philippe@philippebeck.net>
2022-04-18 21:34:59 +0200

    Extract specific code from NavElt to App

--- ui/src/components/NavElt.vue
+++ ui/src/components/NavElt.vue
@@ -12,80 +12,9 @@
       type="radio" 
       name="navbar">
 
-    <a 
-      class="anima-slideB-this" 
-      href="/" 
-      title="Accueil">
-      <img 
-        src="logo.svg" 
-        alt="Logo Phi" 
-        width="30" 
-        height="30">
-    </a>
-
-    <ul>
-      <li>
-        <a 
-          href="/">
-          <i class="fa-solid fa-link fa-fw"></i>
-          <strong>Liens</strong>
-        </a>
-      </li>
-      <li>
-        <a 
-          href="/contact">
-          <i class="fa-brands fa-at fa-fw"></i>
-          <strong>Contact</strong>
-        </a>
-      </li>
-    </ul>
-
-    <ul v-if="!userId">
-      <li>
-        <a 
-          class="onto-color-cyan" 
-          href="https://paypal.me/philippebeck" 
-          title="Donation">
-          <i class="fa-brands fa-paypal fa-lg fa-fw"></i>
-        </a>
-      </li>
-      <li>
-        <a 
-          class="onto-color-pink" 
-          href="https://github.com/sponsors/philippebeck" 
-          title="Sponsor">
-          <i class="far fa-heart fa-lg fa-fw"></i>
-        </a>
-      </li>
-      <li>
-        <a 
-          class="onto-color-red" 
-          href="/login" 
-          title="Login">
-          <i class="fa-solid fa-sign-in-alt fa-lg fa-fw"></i>
-        </a>
-      </li>
-    </ul>
-
-    <ul v-else>
-      <li>
-        <a 
-          class="onto-color-red" 
-          href="/admin" 
-          title="Admin">
-          <i class="fa-solid fa-user-ninja fa-lg fa-fw"></i>
-        </a>
-      </li>
-      <li>
-        <button 
-          class="onto-color-red" 
-          type="button"
-          @click="logout()"
-          title="Logout">
-          <i class="fa-solid fa-sign-out-alt fa-lg fa-fw"></i>
-        </button>
-      </li>
-    </ul>
+    <slot name="brand"></slot>
+    <slot name="nav"></slot>
+    <slot name="admin"></slot>
     
     <label 
       for="navbar-close" 
@@ -102,26 +31,7 @@
 
 <script>
 export default {
-  name: "NavElt",
-
-  data() {
-    return {
-      userId: null
-    }
-  },
-
-  methods: {
-    logout() {
-      localStorage.clear();
-      this.$router.push("/");
-    }
-  },
-  
-  mounted() {
-    if (localStorage.userId) {
-      this.userId = JSON.parse(localStorage.userId);
-    }
-  }
+  name: "NavElt"
 }
 </script>
 
