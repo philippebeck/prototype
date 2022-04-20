b0be2bab3b439fdac97997410b2062cc13fc6df1
Philippe Beck <philippe@philippebeck.net>
2022-04-20 12:58:29 +0200

    Merge & move all services

--- ui/src/script/services.js
+++ ui/src/script/services.js
@@ -1,6 +1,83 @@
-"use strict";
+"mode strict";
 
-import * as constants from "../config/constants";
+import * as constants from "./constants";
+
+const axios = require("axios");
+
+/**
+ * SET AXIOS DEFAULTS
+ */
+export function setAxios() {
+  axios.defaults.baseURL = constants.API_URL;
+  axios.defaults.headers.post["Content-Type"] = constants.CONTENT_TYPE;
+  
+  if (constants.TOKEN) {
+    axios.defaults.headers.common["Authorization"] = "Bearer " + constants.TOKEN;
+  }
+}
+
+/**
+ * GET DATA
+ * @param {string} url 
+ * @returns 
+ */
+export async function getData(url) {
+  setAxios();
+  const response = await axios.get(url);
+
+  return response.data;
+}
+
+/**
+ * POST DATA
+ * @param {string} url 
+ * @param {array} data 
+ * @returns 
+ */
+export async function postData(url, data) {
+  setAxios();
+  const response = await axios.post(url, data);
+
+  return response.data;
+}
+
+/**
+ * PATCH DATA
+ * @param {string} url 
+ * @param {array} data 
+ * @returns 
+ */
+export async function patchData(url, data) {
+  setAxios();
+  const response = await axios.patch(url, data);
+
+  return response.data;
+}
+
+/**
+ * PUT DATA
+ * @param {string} url 
+ * @param {array} data 
+ * @returns 
+ */
+export async function putData(url, data) {
+  setAxios();
+  const response = await axios.put(url, data);
+
+  return response.data;
+}
+
+/**
+ * DELETE DATA
+ * @param {string} url 
+ * @returns 
+ */
+export async function deleteData(url) {
+  setAxios();
+  const response = await axios.delete(url);
+
+  return response.data;
+}
 
 /**
  * CHECK STRING
