159baa98253dc8942afa991e49d6c712d706bc7c
Philippe Beck <philippe@philippebeck.net>
2022-05-01 22:01:12 +0200

    Implement CardElt to ContactView

--- ui/src/views/ContactView.vue
+++ ui/src/views/ContactView.vue
@@ -1,97 +1,104 @@
 <template>
-  <main>
-    <h1 class="violet anima-slideB">
-      <i class="fa-solid fa-at fa-2x"></i>
-      Contact
-    </h1>
-    <form class="anima-grow">
-      <ul>
-        <li>
-          <FieldElt
-            id="name"
-            v-model:value="name"
-            info="Indiquer votre nom"
-            required>
-            <template #legend>
-              Nom
-            </template>
-            <template #label>
-              Nom et prénom
-            </template>
-          </FieldElt>
-        </li>
-        <li>
-          <FieldElt
-            id="email"
-            v-model:value="email"
-            info="Indiquer votre email"
-            type="email"
-            required>
-            <template #legend>
-              Email
-            </template>
-            <template #label>
-              Un email valide svp !
-            </template>
-          </FieldElt>
-        </li>
-        <li>
-          <FieldElt
-            id="title"
-            v-model:value="title"
-            info="Indiquer le titre"
-            required>
-            <template #legend>
-              Titre
-            </template>
-            <template #label>
-              La raison de ce message ?
-            </template>
-          </FieldElt>
-        </li>
-        <li>
-          <FieldElt
-            id="message"
-            v-model:value="message"
-            info="Indiquer votre message"
-            type="area"
-            required>
-            <template #legend>
-              Message
-            </template>
-            <template #label>
-              Soyez bref et précis !
-            </template>
-          </FieldElt>
-        </li>
-        <li>
-          <div 
-            id="recaptcha" 
-            class="g-recaptcha" 
-            data-sitekey="6LdTBtoZAAAAADITfTTXpjsctFXZqKXZc-seM9ZL">
-          </div>
-        </li>
-        <li>
-          <BtnElt
-            type="button"
-            content="Envoyer"
-            @click="send()" 
-            class="btn-green"/>
-        </li>
-      </ul>
-    </form>
-  </main>
+  <CardElt>
+    <template #head>
+      <h1 class="violet anima-slideB">
+        <i class="fa-solid fa-at fa-2x"></i>
+        Contact
+      </h1>
+    </template>
+
+    <template #body>
+      <form class="anima-grow">
+        <ul>
+          <li>
+            <FieldElt
+              id="name"
+              v-model:value="name"
+              info="Indiquer votre nom"
+              required>
+              <template #legend>
+                Nom
+              </template>
+              <template #label>
+                Nom et prénom
+              </template>
+            </FieldElt>
+          </li>
+          <li>
+            <FieldElt
+              id="email"
+              v-model:value="email"
+              info="Indiquer votre email"
+              type="email"
+              required>
+              <template #legend>
+                Email
+              </template>
+              <template #label>
+                Un email valide svp !
+              </template>
+            </FieldElt>
+          </li>
+          <li>
+            <FieldElt
+              id="title"
+              v-model:value="title"
+              info="Indiquer le titre"
+              required>
+              <template #legend>
+                Titre
+              </template>
+              <template #label>
+                La raison de ce message ?
+              </template>
+            </FieldElt>
+          </li>
+          <li>
+            <FieldElt
+              id="message"
+              v-model:value="message"
+              info="Indiquer votre message"
+              type="area"
+              required>
+              <template #legend>
+                Message
+              </template>
+              <template #label>
+                Soyez bref et précis !
+              </template>
+            </FieldElt>
+          </li>
+          <li>
+            <div 
+              id="recaptcha" 
+              class="g-recaptcha" 
+              data-sitekey="6LdTBtoZAAAAADITfTTXpjsctFXZqKXZc-seM9ZL">
+            </div>
+          </li>
+          <li>
+            <BtnElt
+              type="button"
+              content="Envoyer"
+              @click="send()" 
+              class="btn-green"/>
+          </li>
+        </ul>
+      </form>
+    </template>
+  </CardElt>
 </template>
 
 <script>
 import BtnElt from '@/components/base/BtnElt';
 import FieldElt from '@/components/base/FieldElt';
+import CardElt from '@/components/main/CardElt';
 import { checkString, rewriteString, postData } from "@/script/services";
 
 export default {
   name: "ContactView",
   components: {
     BtnElt,
+    CardElt,
     FieldElt
   },
   data() {
